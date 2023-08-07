858e33237c494ff7f36e42a2c7fd882056d77755
jonesna213 <navyjones9@gmail.com>
2023-08-07 16:06:44 -0500

    Started building header and built footer. Also added bootstrap to the project to make styling easier

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Navy Jones's Portfolio Website
+
+### This website was build in React.js and deployed to AWS with the url https://www.navyjones.com
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    return (
+        <header>
+            <div>
+                
+            </div>
+            <div >
+                <ul className="d-flex">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
