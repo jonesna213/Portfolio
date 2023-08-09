7513595f41624ffbb729af82f2dafa7ad69a7413
jonesna213 <navyjones9@gmail.com>
2023-08-09 14:56:01 -0500

    Finished as of now the header/navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,49 @@
+import { NavLink } from "react-router-dom";
+import image from "../assets/pic.jpg";
+import styles from "./Navigation.module.css";
+import { useState } from "react";
+
+const Navigation = () => {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen);
+    };
+
+    return (
+        <nav className={`navbar navbar-expand-md navbar-light bg-light ${styles.navbar}`}>
+            <div className="container">
+                <a className="navbar-brand" href="/">
+                    <img src={image} alt="Navy Jones" className={`rounded-circle ${styles.pic}`} />
+                    <span className="mx-3">Navy Jones</span>
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    onClick={toggleNav}
+                    aria-expanded={isNavOpen}
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className={`collapse navbar-collapse ${isNavOpen ? 'show' : ''}`}>
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <NavLink to="/" className={({ isActive }) => isActive ? styles.links + " " + styles.active : styles.links} end>Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/about" className={({ isActive }) => isActive ? styles.links + " " + styles.active : styles.links}>About</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/projects" className={({ isActive }) => isActive ? styles.links + " " + styles.active : styles.links}>Projects</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/contact" className={({ isActive }) => isActive ? styles.links + " " + styles.active : styles.links}>Contact</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navigation;
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
+import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 
 const RootLayout = () => {
     return (
         <>
-            <Header />
+            <Navigation />
             <main>
                 <Outlet />
             </main>
