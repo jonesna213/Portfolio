db36c18bb77c41d8d6bcce2d96e8021dd7c9b47f
jonesna213 <navyjones9@gmail.com>
2023-08-09 15:04:39 -0500

    Fixed error page so it has navigation and some styling (might come back to later)

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,15 @@
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+
 const ErrorPage = () => {
     return (
         <>
-            <h1>An error occurred!</h1>
-            <p>Could not find this page!</p>
+            <Navigation />
+            <main className="text-center text-danger my-5">
+                <h1>An error occurred!</h1>
+                <p>Could not find this page!</p>
+            </main>
+            <Footer />
         </>
     );
 }
