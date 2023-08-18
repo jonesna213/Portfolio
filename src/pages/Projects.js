a1e0f635dbc4f0733679e7c90656587693778ea5
jonesna213 <navyjones9@gmail.com>
2023-08-17 20:48:50 -0500

    Started projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,12 @@
+import styles from "./css/Contact.module.css";
+
 const ProjectsPage = () => {
-    return <h1>Projects Page!</h1>;
+    return (
+        <div className={styles.container}>
+            <h1 className="fw-bold text-center">Contact Me</h1>
+            <div className={`mx-auto my-4 ${styles.customHr}`}></div>
+        </div>
+    );
 }
 
 export default ProjectsPage;
