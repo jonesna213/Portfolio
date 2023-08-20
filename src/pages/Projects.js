5281ef111c7df326e241e2360ac3b78bb675020e
jonesna213 <navyjones9@gmail.com>
2023-08-20 15:11:35 -0500

    Trying to fix aws build settings

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,6 +6,11 @@ const ProjectsPage = () => {
             <h1 className="fw-bold text-center">My Projects</h1>
             <div className={`mx-auto my-4 ${styles.customHr}`}></div>
             <p className={`text-center w-50 mx-auto ${styles.text}`}>Here are some of my favorite projects. Feel free to check out the code on github as well!</p>
+
+            <div>
+                <img src="" alt="" />
+                <p></p>
+            </div>
         </div>
     );
 }
