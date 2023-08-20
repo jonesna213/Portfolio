662edc2a5af1a686d16b066105b1e8c8c37cb65e
jonesna213 <navyjones9@gmail.com>
2023-08-20 15:06:53 -0500

    added text in projects file

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,6 +5,7 @@ const ProjectsPage = () => {
         <div className={styles.container}>
             <h1 className="fw-bold text-center">My Projects</h1>
             <div className={`mx-auto my-4 ${styles.customHr}`}></div>
+            <p className={`text-center w-50 mx-auto ${styles.text}`}>Here are some of my favorite projects. Feel free to check out the code on github as well!</p>
         </div>
     );
 }
